5f33e7fdf37596b5b055663410dbb4e900bbd8ae
ian15963 <ian.santos@sptech.school>
2025-01-07 12:29:09 -0300

    Initial commit.

---
+++ src/main/java/com/batch/base_batch/BaseBatchApplication.java
@@ -0,0 +1,13 @@
+package com.batch.base_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch/base_batch/BaseBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.base_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
