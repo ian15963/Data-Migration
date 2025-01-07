8a812e26dcf1b0225b850311a3d9ef22e12b7f98
ian15963 <ian.santos@sptech.school>
2025-01-07 14:27:11 -0300

    feat: Adding different datasources.

---
+++ src/main/java/com/batch/base_batch/config/DataSourceConfig.java
@@ -0,0 +1,29 @@
+package com.batch.base_batch.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Primary
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource batchDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "app.datasource")
+    public DataSource appDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+
+
+}
