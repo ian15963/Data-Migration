68d31b2584c7369e25e6c88130667ca94213949c
ian15963 <ian.santos@sptech.school>
2025-01-07 13:28:03 -0300

    feat: Creating job.

---
+++ src/main/java/com/batch/base_batch/job/DataMigrationJob.java
@@ -0,0 +1,28 @@
+package com.batch.base_batch.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataMigrationJob {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Bean
+    public Job migrationJob(Step pessoaMigrationStep,
+                            Step dadosBancariosMigrationStep){
+        return new JobBuilder("migrationJob", jobRepository)
+                .start(pessoaMigrationStep)
+                .next(dadosBancariosMigrationStep)
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+}
